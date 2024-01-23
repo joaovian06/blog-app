d423f54e7a6be93c5cb99ba61f8f56521abc4279
joaovian06 <joaovitor_vian@hotmail.com>
2024-01-23 15:43:29 -0300

    feat: add sign up functionality and enable cors

--- client/src/components/sign-up/index.jsx
+++ client/src/components/sign-up/index.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../assets/shared/styles/signInSignUpStyles.css";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const onHandleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("/auth", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+        password_confirmation: confirmPassword,
+      }),
+    });
+    console.log(response.json());
+  };
+
   return (
     <div className="container">
       <div className="header">
@@ -12,27 +36,49 @@ const SignUp = () => {
       <div className="sign-up-fields">
         <div className="input">
           <i className="fa-regular fa-user fa-xl"></i>
-          <input type="text" placeholder="Name" />
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         <div className="input">
           <i className="fa-regular fa-envelope fa-xl"></i>
-          <input type="email" placeholder="E-mail" />
+          <input
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
         <div className="input">
           <i className="fa-solid fa-lock fa-xl"></i>
-          <input type="password" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
         <div className="input">
           <i className="fa-solid fa-lock fa-xl"></i>
-          <input type="password" placeholder="Password Confirmation" />
+          <input
+            type="password"
+            placeholder="Password Confirmation"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
         </div>
       </div>
 
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
+        <div className="submit" onClick={onHandleSubmit}>
+          Sign Up
+        </div>
       </div>
     </div>
   );
